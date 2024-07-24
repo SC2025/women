761f4b9b20b127c9e862de9d410ea74ef638c04a
SC2025 <168258416+SC2025@users.noreply.github.com>
2024-07-24 11:10:06 +0530

    Update script_photo.js

--- script_photo.js
+++ script_photo.js
@@ -1,143 +1,142 @@
-  <!-- product photo change function -->
+// product photo change function 
 
 
             
-    var slideWK3Index = 0;
-                
-                function showSlidesWK3() {
-                  let i;
-                  let slidesWK3 = document.getElementsByClassName("mySlidesWK3");
-                  let dotsWK3 = document.getElementsByClassName("dotWK3");
-                  for (i = 0; i < slidesWK3.length; i++) {
-                    slidesWK3[i].style.display = "none";  
-                  }
-                  slideWK3Index++;
-                  if (slideWK3Index > slidesWK3.length) {slideWK3Index = 1}    
-                  for (i = 0; i < dotsWK3.length; i++) {
-                    dotsWK3[i].className = dotsWK3[i].className.replace(" active", "");
-                  }
-                  slidesWK3[slideWK3Index-1].style.display = "block";  
-                  dotsWK3[slideWK3Index-1].className += " active";
-                  setTimeout(showSlidesWK3, 2000); // Change image every 2 seconds
-                };
-                showSlidesWK3();
-                
-                
-                
-                
-                
-                
-                
-                
-    
-                
-                var slideWK7Index = 0;
-                
-                function showSlidesWK7() {
-                  let i;
-                  let slidesWK7 = document.getElementsByClassName("mySlidesWK7");
-                  let dotsWK7 = document.getElementsByClassName("dotWK7");
-                  for (i = 0; i < slidesWK7.length; i++) {
-                    slidesWK7[i].style.display = "none";  
-                  }
-                  slideWK7Index++;
-                  if (slideWK7Index > slidesWK7.length) {slideWK7Index = 1}    
-                  for (i = 0; i < dotsWK7.length; i++) {
-                    dotsWK7[i].className = dotsWK7[i].className.replace(" active", "");
-                  }
-                  slidesWK7[slideWK7Index-1].style.display = "block";  
-                  dotsWK7[slideWK7Index-1].className += " active";
-                  setTimeout(showSlidesWK7, 2000); // Change image every 2 seconds
-                };
-                showSlidesWK7();
-                
-                
-                var slideWK8Index = 0;
-                
-                function showSlidesWK8() {
-                  let i;
-                  let slidesWK8 = document.getElementsByClassName("mySlidesWK8");
-                  let dotsWK8 = document.getElementsByClassName("dotWK8");
-                  for (i = 0; i < slidesWK8.length; i++) {
-                    slidesWK8[i].style.display = "none";  
-                  }
-                  slideWK8Index++;
-                  if (slideWK8Index > slidesWK8.length) {slideWK8Index = 1}    
-                  for (i = 0; i < dotsWK8.length; i++) {
-                    dotsWK8[i].className = dotsWK8[i].className.replace(" active", "");
-                  }
-                  slidesWK8[slideWK8Index-1].style.display = "block";  
-                  dotsWK8[slideWK8Index-1].className += " active";
-                  setTimeout(showSlidesWK8, 2000); // Change image every 2 seconds
-                };
-                showSlidesWK8();
-                
-                
-                var slideWK9Index = 0;
-    
-    function showSlidesWK9() {
-      let i;
-      let slidesWK9 = document.getElementsByClassName("mySlidesWK9");
-      let dotsWK9 = document.getElementsByClassName("dotWK9");
-      for (i = 0; i < slidesWK9.length; i++) {
-        slidesWK9[i].style.display = "none";  
-      }
-      slideWK9Index++;
-      if (slideWK9Index > slidesWK9.length) {slideWK9Index = 1}    
-      for (i = 0; i < dotsWK9.length; i++) {
-        dotsWK9[i].className = dotsWK9[i].className.replace(" active", "");
-      }
-      slidesWK9[slideWK9Index-1].style.display = "block";  
-      dotsWK9[slideWK9Index-1].className += " active";
-      setTimeout(showSlidesWK9, 2000); // Change image every 2 seconds
-    }
-    
-    showSlidesWK9();
-                
-    
-    
-    
-    var slideWK10Index = 0;
-    
-    function showSlidesWK10() {
-      let i;
-      let slidesWK10 = document.getElementsByClassName("mySlidesWK10");
-      let dotsWK10 = document.getElementsByClassName("dotWK10");
-      for (i = 0; i < slidesWK10.length; i++) {
-        slidesWK10[i].style.display = "none";  
-      }
-      slideWK10Index++;
-      if (slideWK10Index > slidesWK10.length) {slideWK10Index = 1}    
-      for (i = 0; i < dotsWK10.length; i++) {
-        dotsWK10[i].className = dotsWK10[i].className.replace(" active", "");
-      }
-      slidesWK10[slideWK10Index-1].style.display = "block";  
-      dotsWK10[slideWK10Index-1].className += " active";
-      setTimeout(showSlidesWK10, 2000); // Change image every 2 seconds
-    };
-    showSlidesWK10();
-    
-    
-    
-    var slideWK13Index = 0;
-    
-    function showSlidesWK13() {
-      let i;
-      let slidesWK13 = document.getElementsByClassName("mySlidesWK13");
-      let dotsWK13 = document.getElementsByClassName("dotWK13");
-      for (i = 0; i < slidesWK13.length; i++) {
-        slidesWK13[i].style.display = "none";  
-      }
-      slideWK13Index++;
-      if (slideWK13Index > slidesWK13.length) {slideWK13Index = 1}    
-      for (i = 0; i < dotsWK13.length; i++) {
-        dotsWK13[i].className = dotsWK13[i].className.replace(" active", "");
-      }
-      slidesWK13[slideWK13Index-1].style.display = "block";  
-      dotsWK13[slideWK13Index-1].className += " active";
-      setTimeout(showSlidesWK13, 2000); // Change image every 2 seconds
-    };
-    showSlidesWK13();
-    
-       
-    
+var slideWK3Index = 0;
+            
+            function showSlidesWK3() {
+              let i;
+              let slidesWK3 = document.getElementsByClassName("mySlidesWK3");
+              let dotsWK3 = document.getElementsByClassName("dotWK3");
+              for (i = 0; i < slidesWK3.length; i++) {
+                slidesWK3[i].style.display = "none";  
+              }
+              slideWK3Index++;
+              if (slideWK3Index > slidesWK3.length) {slideWK3Index = 1}    
+              for (i = 0; i < dotsWK3.length; i++) {
+                dotsWK3[i].className = dotsWK3[i].className.replace(" active", "");
+              }
+              slidesWK3[slideWK3Index-1].style.display = "block";  
+              dotsWK3[slideWK3Index-1].className += " active";
+              setTimeout(showSlidesWK3, 2000); // Change image every 2 seconds
+            };
+            showSlidesWK3();
+            
+            
+            
+            
+            
+            
+            
+            
+
+            
+            var slideWK7Index = 0;
+            
+            function showSlidesWK7() {
+              let i;
+              let slidesWK7 = document.getElementsByClassName("mySlidesWK7");
+              let dotsWK7 = document.getElementsByClassName("dotWK7");
+              for (i = 0; i < slidesWK7.length; i++) {
+                slidesWK7[i].style.display = "none";  
+              }
+              slideWK7Index++;
+              if (slideWK7Index > slidesWK7.length) {slideWK7Index = 1}    
+              for (i = 0; i < dotsWK7.length; i++) {
+                dotsWK7[i].className = dotsWK7[i].className.replace(" active", "");
+              }
+              slidesWK7[slideWK7Index-1].style.display = "block";  
+              dotsWK7[slideWK7Index-1].className += " active";
+              setTimeout(showSlidesWK7, 2000); // Change image every 2 seconds
+            };
+            showSlidesWK7();
+            
+            
+            var slideWK8Index = 0;
+            
+            function showSlidesWK8() {
+              let i;
+              let slidesWK8 = document.getElementsByClassName("mySlidesWK8");
+              let dotsWK8 = document.getElementsByClassName("dotWK8");
+              for (i = 0; i < slidesWK8.length; i++) {
+                slidesWK8[i].style.display = "none";  
+              }
+              slideWK8Index++;
+              if (slideWK8Index > slidesWK8.length) {slideWK8Index = 1}    
+              for (i = 0; i < dotsWK8.length; i++) {
+                dotsWK8[i].className = dotsWK8[i].className.replace(" active", "");
+              }
+              slidesWK8[slideWK8Index-1].style.display = "block";  
+              dotsWK8[slideWK8Index-1].className += " active";
+              setTimeout(showSlidesWK8, 2000); // Change image every 2 seconds
+            };
+            showSlidesWK8();
+            
+            
+            var slideWK9Index = 0;
+
+function showSlidesWK9() {
+  let i;
+  let slidesWK9 = document.getElementsByClassName("mySlidesWK9");
+  let dotsWK9 = document.getElementsByClassName("dotWK9");
+  for (i = 0; i < slidesWK9.length; i++) {
+    slidesWK9[i].style.display = "none";  
+  }
+  slideWK9Index++;
+  if (slideWK9Index > slidesWK9.length) {slideWK9Index = 1}    
+  for (i = 0; i < dotsWK9.length; i++) {
+    dotsWK9[i].className = dotsWK9[i].className.replace(" active", "");
+  }
+  slidesWK9[slideWK9Index-1].style.display = "block";  
+  dotsWK9[slideWK9Index-1].className += " active";
+  setTimeout(showSlidesWK9, 2000); // Change image every 2 seconds
+}
+
+showSlidesWK9();
+            
+
+
+
+var slideWK10Index = 0;
+
+function showSlidesWK10() {
+  let i;
+  let slidesWK10 = document.getElementsByClassName("mySlidesWK10");
+  let dotsWK10 = document.getElementsByClassName("dotWK10");
+  for (i = 0; i < slidesWK10.length; i++) {
+    slidesWK10[i].style.display = "none";  
+  }
+  slideWK10Index++;
+  if (slideWK10Index > slidesWK10.length) {slideWK10Index = 1}    
+  for (i = 0; i < dotsWK10.length; i++) {
+    dotsWK10[i].className = dotsWK10[i].className.replace(" active", "");
+  }
+  slidesWK10[slideWK10Index-1].style.display = "block";  
+  dotsWK10[slideWK10Index-1].className += " active";
+  setTimeout(showSlidesWK10, 2000); // Change image every 2 seconds
+};
+showSlidesWK10();
+
+
+
+var slideWK13Index = 0;
+
+function showSlidesWK13() {
+  let i;
+  let slidesWK13 = document.getElementsByClassName("mySlidesWK13");
+  let dotsWK13 = document.getElementsByClassName("dotWK13");
+  for (i = 0; i < slidesWK13.length; i++) {
+    slidesWK13[i].style.display = "none";  
+  }
+  slideWK13Index++;
+  if (slideWK13Index > slidesWK13.length) {slideWK13Index = 1}    
+  for (i = 0; i < dotsWK13.length; i++) {
+    dotsWK13[i].className = dotsWK13[i].className.replace(" active", "");
+  }
+  slidesWK13[slideWK13Index-1].style.display = "block";  
+  dotsWK13[slideWK13Index-1].className += " active";
+  setTimeout(showSlidesWK13, 2000); // Change image every 2 seconds
+};
+showSlidesWK13();
+
+   
